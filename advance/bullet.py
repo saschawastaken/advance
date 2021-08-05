94b66dab5aeb80aaa77d770f2bfa99619b2dbc13
saschawastaken <83161969+saschawastaken@users.noreply.github.com>
2021-08-05 19:04:58 +0200

    Add files via upload

--- advance/bullet.py
+++ advance/bullet.py
@@ -1,19 +1,23 @@
 import pygame
+import math
 
 class Bullet():
 
-    def __init__(self, pos, vel_x, vel_y) -> None:
+    def __init__(self, pos, vel_x, vel_y, imgAngle) -> None:
         # GRAPHICS
-        self.color = (255,255,0)
+        self.img = pygame.image.load('img/bullet.png')
+        #self.img.set_colorkey((0,0,0))
         self.pos = pos
         # MOVEMENT
         self.vel_x = vel_x
         self.vel_y = vel_y
+        self.imgAngle = imgAngle
 
     def show(self, win, rows):
         size = win.get_width() / rows
-        pygame.draw.rect(win, self.color, (self.pos[0] * size, self.pos[1] * size, size, size))
+        img_copy = pygame.transform.rotate(self.img, self.imgAngle)
+        win.blit(img_copy, (self.pos[0] * size - int(img_copy.get_width() / 2) , self.pos[1] * size - int(img_copy.get_height() / 2)))
 
-    def move(self):
+    def move(self, win, mousePos, rows):
         self.pos[0] -= self.vel_x
         self.pos[1] -= self.vel_y
--- advance/player.py
+++ advance/player.py
@@ -1,6 +1,8 @@
 #
 import pygame
 import math
+
+from pygame import mouse
 #
 import bullet
 
@@ -9,7 +11,7 @@ class Player():
     def __init__(self, pos, imgURL) -> None:
         # *Key elements marked with k at beginning
         self.pos = pos
-        self.angle = 0
+        self.imgAngle = 0
         # GRAPHICAL
         self.img = pygame.image.load(imgURL)
         self.color = (0,255,0)
@@ -21,7 +23,7 @@ class Player():
         
     def show(self, win, rows):
         size = win.get_width() / rows
-        img_copy = pygame.transform.rotate(self.img, self.angle)
+        img_copy = pygame.transform.rotate(self.img, self.imgAngle)
         win.blit(img_copy, (self.pos[0] * size - int(img_copy.get_width() / 2) , self.pos[1] * size - int(img_copy.get_height() / 2 )))
         for bullet in self.bullets:
             bullet.show(win, rows)
@@ -34,15 +36,23 @@ class Player():
         opposite_leg = mx - self.pos[0] * size
         adjacent_leg = self.pos[1] * size - my
 
-        if adjacent_leg > 0: self.angle = 90 - math.degrees(math.atan(opposite_leg / adjacent_leg))
+        if adjacent_leg > 0: self.imgAngle = 90 - math.degrees(math.atan(opposite_leg / adjacent_leg))
 
         for bullet in self.bullets:
-            bullet.move()
+            bullet.move(win, mousePos, rows)
 
-    def shoot(self):
+    def shoot(self, win, mousePos, rows):
         if self.kShoot:
             pos_x, pos_y = self.pos
-            self.bullets.append(bullet.Bullet([pos_x, pos_y], 0, 1))
+            mx, my = mousePos
+            size = win.get_width() / rows
+
+            opposite_leg = mx - self.pos[0] * size
+            adjacent_leg = self.pos[1] * size - my
+
+            if adjacent_leg > 0: self.bullets.append(bullet.Bullet([pos_x, pos_y], (opposite_leg / adjacent_leg) * -1, 1, self.imgAngle + 90))
+
+            self.kShoot = False
 
     def build(self):
         pass
