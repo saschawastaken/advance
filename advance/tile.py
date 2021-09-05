e0783cb015f0641557d0323b270a39b597947b31
saschawastaken <83161969+saschawastaken@users.noreply.github.com>
2021-09-05 21:38:48 +0200

    Delete advance directory

--- advance/tile.py
+++
@@ -1,8 +0,0 @@
-import pygame
-class Tile():
-    def __init__(self, pos) -> None:
-        self.pos = pos
-        self.color = (139,69,19)
-
-    def show(self, win, size):
-        pygame.draw.rect(win, self.color, (self.pos[0] * size, self.pos[1] * size, size, size))
