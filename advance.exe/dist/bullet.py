14e84c46e12ba6334329c16d7f323565d8916ac4
saschawastaken <83161969+saschawastaken@users.noreply.github.com>
2021-10-16 00:01:12 +0200

    Add files via upload

---
+++ advance.exe/dist/bullet.py
@@ -0,0 +1,21 @@
+import pygame
+
+class Bullet():
+
+    def __init__(self, pos, vel_x, vel_y, imgAngle) -> None:
+        self.img = pygame.image.load('img/bullet.png')
+        self.imgSize = [3,10]
+        self.pos = pos
+        self.rect = pygame.Rect(0,0,0,0)
+        self.imgAngle = imgAngle
+        self.vel_x = vel_x
+        self.vel_y = vel_y
+
+    def show(self, win, size):
+        img_copy = pygame.transform.rotate(self.img, self.imgAngle)
+        self.rect = pygame.Rect(self.pos[0] * size - int(img_copy.get_width() / 2) , self.pos[1] * size - int(img_copy.get_height() / 2), self.imgSize[0], self.imgSize[1])
+        win.blit(img_copy, self.rect)
+
+    def move(self):
+        self.pos[0] -= self.vel_x / 3
+        self.pos[1] -= self.vel_y / 3
